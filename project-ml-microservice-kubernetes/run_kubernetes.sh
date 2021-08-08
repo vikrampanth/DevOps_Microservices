c8298d15bdbbe458be4af2c1cf624ed80ad777f8
Vikram Panth <vikrampanth@gmail.com>
2021-08-08 12:07:27 +0530

    test

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="noahgift/flasksklearn"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flaskskearlndemo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=flaskskearlndemo
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward flaskskearlndemo 8000:80
